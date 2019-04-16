e9d39cc428798b534f4f13b2d0da545f380b873a
jascholz <jascholz@ebay.com>
2019-04-16 11:38:01 +0200

    add cors

--- src/app.js
+++ src/app.js
@@ -4,9 +4,11 @@ const bodyParser = require("body-parser");
 const morgan = require("morgan");
 const requireAll = require("require-dir-all");
 const Sequelize = require('sequelize');
+const cors = require('cors');
 
 module.exports = ({storage = "database.sqlite"} = {}) => {
     app.use(bodyParser.json());
+    app.use(cors());
 
     app.use(morgan("dev"));
 
