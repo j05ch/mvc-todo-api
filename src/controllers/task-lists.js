903827d85ccbe580b69b30f37040f2ad47e6eb5d
jascholz <jascholz@ebay.com>
2019-05-11 16:09:50 +0200

    add tasklist delete controller

--- src/controllers/task-lists.js
+++ src/controllers/task-lists.js
@@ -13,3 +13,16 @@ controller.post('/task-lists', async function(req, res) {
     const taskList = await TaskList.create(req.body, { fields: [ 'title' ] });
     res.status(201).json(taskList);
 });
+
+controller.delete('/task-lists/:taskListsId', async function (req, res) {
+    const taskList = await TaskList.findOne({
+        where: {
+            id: req.params.taskListId,
+        }
+    });
+    if (taskList) {
+        await taskList.destroy();
+        res.status(200).json(taskList);
+    } else
+        res.sendStatus(404);
+});
