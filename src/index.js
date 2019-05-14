257d2575bc73494e5bc1d6eb44b79b39fa78cc41
jascholz <jascholz@ebay.com>
2019-05-14 14:23:04 +0200

    refactor

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 const app = require('./app')({dbUrl: process.env.DATABASE_URL});
 
 const port = process.env.PORT || 3000;
-// Paramater Objekt sync ({force: true})
 global.sequelize.sync().then(() => {
     app.listen(port, () => {
         console.log(`MVC app listening on port ${port}!`);
